a4e62950f2e36982ff73f7ba808d183ea50b119f
anhnguyen <nguyendanghaianh1233@gmai.com>
2025-05-13 23:59:38 +0700

    add simple mcp server support sse

---
+++ src/main/java/io/condutkor/mcpserver/WeatherService.java
@@ -0,0 +1,13 @@
+package io.condutkor.mcpserver;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+
+    @Tool(description = "Get weather information by city name")
+    public String getWeather(String cityName) {
+        return "Weather for " + cityName;
+    }
+}
